6de6edc72004c2983efaf0568b32332df3e15435
VyacheslavCHik <asyofka@mail.ru>
2022-04-27 08:19:39 +0300

    Домашнее задание урока 1.10. Исправлено 03

--- src/Author.java
+++ src/Author.java
@@ -3,14 +3,12 @@ import java.util.Objects;
 public class Author {
     private String name;
     private String lastName;
-    private int dateOfBirth;
-    private String placeOfBirth;
+
 
     public Author(String name, String lastName, int dateOfBirth, String placeOfBirth) {
         this.name = name;
         this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.placeOfBirth = placeOfBirth;
+
     }
 
 
@@ -22,30 +20,24 @@ public class Author {
         return this.lastName;
     }
 
-    public int getDateOfBirth() {
-        return this.dateOfBirth;
-    }
 
-    public String getPlaceOfBirth() {
-        return this.placeOfBirth;
-    }
 
     @Override
     public boolean equals(Object other) {
         if (this == other) return true;
         if (other == null || getClass() != other.getClass()) return false;
         Author author = (Author) other;
-        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName) && Objects.equals(placeOfBirth, author.placeOfBirth) && Objects.equals(dateOfBirth, author.dateOfBirth);
+        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, lastName, placeOfBirth);
+        return Objects.hash(name, lastName);
     }
 
     @Override
     public String toString() {
-        return "Writer: " + name + " " + lastName + ". Was born in " + dateOfBirth + " year, in the town " + placeOfBirth + ".";
+        return  name + " " + lastName;
     }
 }
 
